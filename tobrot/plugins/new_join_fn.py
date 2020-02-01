c3153ba152f33c8f85213b259ffa39ac412a0aa6
NOPMS <55627565+NOPMS@users.noreply.github.com>
2020-02-02 04:56:58 +0800

    some more tweaks

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -33,7 +33,7 @@ async def new_join_f(client, message):
 
 
 async def help_message_f(client, message):
-    await message.reply_text("no one gonna help you 🤣🤣🤣🤣", quote=True)
+    await message.reply_text("No One Gonna Help You 🤣🤣🤣🤣", quote=True)
     channel_id = str(AUTH_CHANNEL)[4:]
     message_id = 99
     # display the /help message
@@ -44,13 +44,13 @@ async def rename_message_f(client, message):
     inline_keyboard = []
     inline_keyboard.append([
         pyrogram.InlineKeyboardButton(
-            text="read this?",
+            text="No Rename Feature For you",
             url="https://t.me/keralagram/698909"
         )
     ])
     reply_markup = pyrogram.InlineKeyboardMarkup(inline_keyboard)
     await message.reply_text(
-        "please use @renamebot",
+        "Please Use @Renamebot",
         quote=True,
         reply_markup=reply_markup
     )
